f5841fe211f2e2df83c2986b5458c36b305206d2
Rocío Calvani <calvanirocio@gmail.com>
2023-08-29 22:26:05 -0300

    mongo url env

--- src/config/MDBSingleton.js
+++ src/config/MDBSingleton.js
@@ -18,7 +18,7 @@ export default class MDBSingleton {
 
     #connectMongoDB = async () => {
         try {
-            await mongoose.connect('mongodb+srv://admin:rocio1@cluster0.facejpa.mongodb.net/Ecommerce')
+            await mongoose.connect(process.env.MONGO_URL)
             console.log("Successful connection to MongoDB");
         } catch (error) {
             console.error("Unable to connect to MongoDB")
